1dd44c053a75f9427f3b14287adc0314b2b74907
RafSchapendonk <rafschapendonk@outlook.com>
2022-11-02 14:39:20 +0100

    debugging

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, jsonify
 from keras.models import load_model
 import numpy as np
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/api/', methods=['POST'])
 def makecalc():
+    model = load_model('../models/final_prediction')
     data = request.get_json()
     prediction = np.array2string(model.predict(data))
 
     return jsonify(prediction)
 
 if __name__ == '__main__':
-    model = load_model('ai-api/models/final_prediction')
-    app.run(debug=True, host='$PORT')
+    app.run(debug=1, host="0.0.0.0", port=os.getenv("PORT") or 5000)
