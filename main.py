124c85e49294d7b732c54aee235252e3eab7a353
RafSchapendonk <rafschapendonk@outlook.com>
2022-11-02 16:53:58 +0100

    first attempt at adding shap

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import base64
+import io
+
 from flask import Flask, request, jsonify
 from keras.models import load_model
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import os
+import shap
 
 app = Flask(__name__)
 
@@ -13,6 +19,30 @@ def makecalc():
 
     return jsonify(prediction)
 
+# def plot_html():
+#     data = request.get_json()
+#     summary = shap.kmeans(X_test, 33)
+#     prediction = np.array2string(model.predict(data))
+#
+#     explainer = shap.KernelExplainer(prediction, summary)
+#     shap_values = explainer.shap_values(data)
+#     shap.force_plot(explainer.expected_value, shap_values[0])
+#
+#     fig = plt.figure()
+#
+#     #
+#     # summary = shap.kmeans(data.X_test, 50)
+#     # e = shap.KernelExplainer(model.predict, summary)
+#     # shap_values = e.shap_values(df_pred)
+#     # shap.summary_plot(shap_values, df_pred, feature_names=df_pred.columns, show=None, plot_type='bar', max_display=10)
+#     #
+#
+#     tmpfile = io.BytesIO()
+#     fig.savefig(tmpfile, format='png')
+#     encoded = base64.b64encode(tmpfile.getbuffer()).decode('ascii')
+#     return f"<img src='data:image/png;base64,{encoded}' alt='Plot unable to load'/>"
+
 if __name__ == '__main__':
+    X_test = pd.read_csv('./X_test.csv')
     model = load_model('models/final_prediction')
     app.run(debug=1, host="0.0.0.0", port=os.getenv("PORT") or 5000)
