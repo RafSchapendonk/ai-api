3e9c796aa5a196ecbc0129b3df86cc2a6c78ea6d
RafSchapendonk <rafschapendonk@outlook.com>
2022-11-02 15:08:44 +0100

    debugging

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 @app.route('/api/', methods=['POST'])
 def makecalc():
-    model = load_model('../models/final_prediction')
     data = request.get_json()
     prediction = np.array2string(model.predict(data))
 
     return jsonify(prediction)
 
 if __name__ == '__main__':
+    model = load_model('models/final_prediction')
     app.run(debug=1, host="0.0.0.0", port=os.getenv("PORT") or 5000)
